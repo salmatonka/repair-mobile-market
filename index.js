b004df349e5e99c5ae4bff314596289d62004c0b
ommes salma <ommes918@gmail.com>
2024-06-21 22:10:33 +0600

    fifth add commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors({origin:'*'}));
+app.use(cors({origin:'http://localhost:5173'}));
 //middleware
 // app.use(cors({
 //   origin: ['https://6675920bff107b952a3e51d6--thunderous-smakager-3ad4c8.netlify.app'],
