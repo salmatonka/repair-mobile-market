a9af086f3a0cf58755f58ea081c55c0f34aa995f
ommes salma <ommes918@gmail.com>
2024-06-21 19:58:24 +0600

    repair commit

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors());
+// app.use(cors());
+//middleware
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
 
 
