38da9520e83893ee0f2d4197e41a819ab245228c
ommes salma <ommes918@gmail.com>
2024-06-21 21:20:01 +0600

    welveth commit

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
-// app.use(cors());
+app.use(cors());
 //middleware
-app.use(cors({
-  origin: ['http://localhost:5173'],
-  credentials: true
-}));
+// app.use(cors({
+//   origin: ['https://6675920bff107b952a3e51d6--thunderous-smakager-3ad4c8.netlify.app'],
+//   credentials: true
+// }));
 app.use(express.json());
 
 
